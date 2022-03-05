66129175239ee7b9b4f48b5b076a80c9c799c599
iwillteixeira <iwill2005@gmail.com>
2022-03-05 15:49:04 -0300

    Fix Linter

--- enumerable.rb
+++ enumerable.rb
@@ -1,26 +1,26 @@
 #!/usr/bin/env ruby
 module MyEnumerable
-    def all?(&block)
-      res = true
-      each do |x|
-        res = false unless block.call(x)
-      end
-      res
+  def all?(&block)
+    res = true
+    each do |x|
+      res = false unless block.call(x)
     end
-  
-    def any?(&block)
-      res = false
-      each do |x|
-        res = true if block.call(x)
-      end
-      res
-    end
-  
-    def filter(&block)
-      res = []
-      each do |x|
-        res << x if block.call(x)
-      end
-      res
-    end
+    res
   end
+
+  def any?(&block)
+    res = false
+    each do |x|
+      res = true if block.call(x)
+    end
+    res
+  end
+
+  def filter(&block)
+    res = []
+    each do |x|
+      res << x if block.call(x)
+    end
+    res
+  end
+end
