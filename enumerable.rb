cdf17e3e3f1b5cca6d56eff8ccfbec6a764b128f
iwillteixeira <iwill2005@gmail.com>
2022-03-05 15:43:09 -0300

    Enumerable

--- enumerable.rb
+++ enumerable.rb
@@ -2,24 +2,24 @@
 module MyEnumerable
     def all?(&block)
       res = true
-      each do |value|
-        res = false unless block.call(value)
+      each do |x|
+        res = false unless block.call(x)
       end
       res
     end
   
     def any?(&block)
       res = false
-      each do |value|
-        res = true if block.call(value)
+      each do |x|
+        res = true if block.call(x)
       end
       res
     end
   
     def filter(&block)
       res = []
-      each do |value|
-        res << value if block.call(value)
+      each do |x|
+        res << x if block.call(x)
       end
       res
     end
